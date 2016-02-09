5672d1d2f60a7925dec521ffed76cf7837a319d6
Samuel Balco <goodlyrottenapple@gmail.com>
2016-02-09 11:07:37 +0000

    semi-working version...

--- Core.hs
+++ Core.hs
@@ -1,16 +1,25 @@
 {-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
 
 module Core where
-import Data.List
-import qualified Data.Map as Map
+import Data.List (intercalate, intersect, sortBy)
+import qualified Data.Map as M
 import Control.Monad (liftM2)
+import Data.Char (toLower)
+import Data.Tuple (swap)
+import Data.Ord (comparing)
+import Data.List.Split (splitOn)
+
+import Debug.Trace
+debug = flip trace
+
 
 type Word = String
 type Tag = String
-type POS = (Core.Word, Core.Tag)
+
+type POS = (Core.Word, [Core.Tag])
 
 instance {-# OVERLAPPING #-} Show POS where
-    show (a,b) = a ++ "|" ++ b
+    show (a,b) = intercalate "|" (a:b)
 
 type Sentence = [POS]
 
@@ -20,69 +29,110 @@ instance {-# OVERLAPPING #-} Show Sentence where
 word :: POS -> Core.Word
 word = fst
 
-tag :: POS -> Core.Tag
-tag = snd
+tags :: POS -> [Core.Tag]
+tags = snd
 
-remPOS :: Sentence -> String
-remPOS = intercalate " " . map word
+
+prod :: [a] -> [b] -> [(a, b)]
+prod = liftM2 (,)
+
+showSentences :: [Sentence] -> String
+showSentences = (intercalate "\n\n") . (map show)
 
 -- adds dummy *start* and *end* tags
 startEndTags :: Sentence -> Sentence
-startEndTags s = (("*start*", "*start*") : s) ++ [("*end*", "*end*")]
-
--- -- counts the occurences of a single tag in a sentence
--- tagOccs :: Tag -> Sentence -> Int
--- tagOccs t = foldl (\acc word -> if t == tag word then acc+1 else acc) 0 
-
--- -- counts the occurences of a single tag in a list of sentences
--- tagOccsL :: Tag -> [Sentence] -> Int
--- tagOccsL t = foldl (\acc s -> acc + tagOccs t s) 0
-
-allTagOccsSentence :: Sentence -> Map.Map Tag Int
-allTagOccsSentence = foldl (\m (w,t) -> Map.insertWith' (+) t 1 m) Map.empty
-
-allTagOccs :: [Sentence] -> Map.Map Tag Int
-allTagOccs = foldl (\m s -> Map.unionWith (+) m (allTagOccsSentence s)) Map.empty
+startEndTags s = (("*start*", ["*start*"]) : s) ++ [("*end*", ["*end*"])]
 
 
--- -- counts the occurences of a two tags following each other in a sentence
--- twoTagsOccs :: Tag -> Tag -> Sentence -> Int
--- twoTagsOccs t1 t2 [] = 0
--- twoTagsOccs t1 t2 [a] = 0
--- twoTagsOccs t1 t2 [a,b] = if t1 == tag a && t2 == tag b then 1 else 0
--- twoTagsOccs t1 t2 (a:b:xs) = (if t1 == tag a && t2 == tag b then 1 else 0) + twoTagsOccs t1 t2 (b:xs)
+type TagCountMap = M.Map Tag Int
+type TagTagCountMap = M.Map (Tag, Tag) Int
+type WordTagCountMap = M.Map (Core.Word, Tag) Int
 
--- -- counts the occurences of a two consecutive tags in a list of sentences
--- twoTagsOccsL :: Tag -> Tag -> [Sentence] -> Int
--- twoTagsOccsL t1 t2 = foldl (\acc s -> acc + twoTagsOccs t1 t2 s) 0
+tagOccsSentence :: Sentence -> TagCountMap
+tagOccsSentence = foldl (\m (w,list) -> foldl (\m' t -> M.insertWith (+) t 1 m') m list) M.empty
 
-
-allTwoTagsOccsSentence :: Sentence -> Map.Map (Tag,Tag) Int
-allTwoTagsOccsSentence [(_,t1),(_,t2)] = Map.fromList [((t1,t2), 1)]
-allTwoTagsOccsSentence (a:b:xs) = Map.insertWith' (+) (tag a, tag b) 1 (allTwoTagsOccsSentence (b:xs))
-allTwoTagsOccsSentence _ = Map.empty
-
-allTwoTagsOccs :: [Sentence] -> Map.Map (Tag,Tag) Int
-allTwoTagsOccs = foldl (\m s -> Map.unionWith (+) m (allTwoTagsOccsSentence s)) Map.empty
+genTagOccsMap :: [Sentence] -> TagCountMap
+genTagOccsMap = foldl (\m s -> M.unionWith (+) m (tagOccsSentence s)) M.empty
 
 
--- -- calculates bigram P(Cat_i|Cat_i−1), with added +1 smoothing
--- pTags :: Tag -> Tag -> [Sentence] -> Float
--- pTags t1 t2 list = (fromIntegral $ twoTagsOccsL t1 t2 list + 1) / (fromIntegral $ tagOccsL t1 list)
+-- generates the occurences of consecutive word tags in (w,[t,t2...,tn]) and (w',[t',t'2...,t'm])
+-- by incerementing the values (t,t'), (t,t'2), ...(tn,t'm) in the map by one
+tagTagOccsSentence :: Sentence -> TagTagCountMap
+tagTagOccsSentence [(_,t1),(_,t2)] = foldl (\m pair -> M.insertWith (+) pair 1 m) M.empty (t1 `prod` t2)
+tagTagOccsSentence (a:b:xs) = foldl (\m pair -> M.insertWith (+) pair 1 m) (tagTagOccsSentence (b:xs)) (tags a `prod` tags b)
+tagTagOccsSentence _ = M.empty
+
+genTagTagOccsMap :: [Sentence] -> TagTagCountMap
+genTagTagOccsMap = foldl (\m s -> M.unionWith (+) m (tagTagOccsSentence s)) M.empty
 
 
--- rmDups :: (Ord a) => [a] -> [a]
--- rmDups = map head . group . sort
+-- folds the list of (Word,[Tag]) pairs into a map indexed by (Word,Tag) pairs.
+wordTagOccsSentence :: Sentence -> WordTagCountMap
+wordTagOccsSentence = foldl (\m (w,list) -> foldl (\m' t -> M.insertWith (+) (lowerCase w,t) 1 m') m list) M.empty where lowerCase = map toLower
 
--- foundTags :: Sentence -> [Core.Tag]
--- foundTags = rmDups . (map tag)
+genWordTagOccsMap :: [Sentence] -> WordTagCountMap
+genWordTagOccsMap = foldl (\m s -> M.unionWith (+) m (wordTagOccsSentence s)) M.empty
 
--- foundTagsL :: [Sentence] -> [Core.Tag]
--- foundTagsL = rmDups . concatMap foundTags
 
--- cartProd :: [a] -> [b] -> [(a, b)]
--- cartProd = liftM2 (,)
 
-pAllTags :: [Sentence] -> Map.Map (Tag, Tag) Float
-pAllTags list = Map.mapWithKey (\(t1,_) count -> (fromIntegral $ count+1) / (fromIntegral $ singleTag Map.! t1)) (allTwoTagsOccsL list)
-    where singleTag = allTagOccsL list
+-- creates a mapping, which for (T2,T1), returns the probability of T1 being followed by T2
+genTagTagProbFun :: TagCountMap -> TagTagCountMap -> ((Tag, Tag) -> Float)
+genTagTagProbFun tagOccsMap tagTagOccsMap = \(j,i) -> (log ((countTT (i,j)) / countT i))
+    where   countTT (i,j) = fromIntegral $ M.findWithDefault 1 (i,j) tagTagOccsMap
+            countT x = fromIntegral $ M.findWithDefault 1 x tagOccsMap
+
+genWordTagProbFun :: TagCountMap -> WordTagCountMap -> ((Core.Word, Tag) -> Float)
+genWordTagProbFun tagOccsMap wordTagOccsMap = \(w,t) -> (log ((countWT (w,t)) / countT t))
+    where   countWT (w,t) = fromIntegral $ M.findWithDefault 1 (w,t) wordTagOccsMap
+            countT x = fromIntegral $ M.findWithDefault 1 x tagOccsMap
+
+
+
+
+tagCountMapToStr :: TagCountMap -> String
+tagCountMapToStr m = intercalate "\n" $ map (\(k,v) -> k ++ " " ++ show v) $ M.toList m
+
+strToTagCountMap :: String -> TagCountMap
+strToTagCountMap = M.fromList . (map (\x -> case splitOn " " x of [a,b] -> (a, read b :: Int))) . lines 
+
+
+strStrCountMapToStr :: M.Map (String, String) Int -> String
+strStrCountMapToStr m = intercalate "\n" $ map (\((t1,t2),v) -> t1 ++ " " ++ t2 ++ " " ++ show v) $ M.toList m
+
+strToStrStrCountMap :: String -> M.Map (String, String) Int
+strToStrStrCountMap = M.fromList . (map (\x -> case splitOn " " x of [a1,a2,b] -> ((a1,a2), read b :: Int))) . lines 
+
+
+-- containsTags :: [Tag] -> Sentence -> Bool
+-- containsTags list s = [] /= filter (([] /=) . (list `intersect`) . tags) s
+
+-- filterSentencesWTags :: [Tag] -> [Sentence] -> [Sentence]
+-- filterSentencesWTags t = filter (containsTags t)
+
+viterbi' :: M.Map Tag Float -> M.Map Tag [Tag] -> ((Core.Word, Tag) -> Float) -> ((Tag, Tag) -> Float) -> [Core.Word] -> [Tag]
+viterbi' score backPtr pWT pTT [] = reverse $ backPtr M.! (findMax score)
+    where findMax = fst . last . sortBy (comparing snd) . M.toList
+viterbi' score backPtr pWT pTT (w:ords) = viterbi' newScore newBackPtr pWT pTT ords `debug` ( w  ++ "\n\n" ++(intercalate "\n" $ map show $ M.toList newScore) ++ "\n\n" ++ (intercalate "\n" $ map show $ M.toList newBackPtr) ++ "\n\n===============================\n\n")
+    where 
+        newScore = M.fromList [(i, snd $ maxK i) | i <- M.keys score]
+        newBackPtr = M.fromList [(i, (fst$maxK i):(backPtr M.! i)) | i <- M.keys backPtr]
+        maxK :: Tag -> (Tag, Float)
+        maxK i = last $ sortBy (comparing snd) [(k, (score M.! k) + (pTT (i, k)) + (pWT (w, i))) | k <- M.keys score]
+
+
+initScore :: ((Core.Word, Tag) -> Float) -> ((Tag, Tag) -> Float) -> Core.Word -> [Tag] -> M.Map Tag Float
+initScore pWT pTT w tags = M.fromList [(t, pWT (w, t) + pTT (t, "*start*")) | t <- tags]
+
+initBackPtr :: [Tag] -> M.Map Tag [Tag]
+initBackPtr tags = M.fromList [(t, []) | t <- tags]
+
+
+viterbi :: [Tag] -> ((Core.Word, Tag) -> Float) -> ((Tag, Tag) -> Float) -> [Core.Word] -> [Tag]
+viterbi tags pWT pTT (w:ords) = viterbi' (initScore pWT pTT w tags) (initBackPtr tags) pWT pTT (ords ++ ["*end*"])
+
+-- score_init :: ((Core.Word, Tag) -> Float) -> ((Tag, Tag) -> Float) -> [Tag] -> Int -> Core.Word -> Matrix Float
+-- score_init pWT pTT tags sentenceLength word = matrix (length tags) sentenceLength fun
+--     where fun = \(i, w) -> if w > 1 then 0.0 else pWT(word, (tags !! (i-1))) + pTT((tags !! (i-1)), "*start*")
+
+
+
--- Parser.hs
+++ Parser.hs
@@ -5,19 +5,35 @@ import Text.Regex.Posix
 import Text.Regex
 import Control.Monad
 import Core
+-- import Debug.Trace
+import Data.List.Split (splitOn)
+import Data.Char (toLower)
+import qualified Data.Map as M
+import Data.Ord (comparing)
+import Data.List (sortBy, intercalate)
+
+-- validTags = ["$", "''", "``", "(", ")", ",", ".", ":", "CC", "CD", "DT", "EX", "FW", "IN", "JJ", "JJR", "JJS", "LS", "MD", "NN", "NNP", "NNPS", "NNS", "PDT", "POS", "PRP", "PRP$", "RB", "RBR", "RBS", "RP", "SYM", "TO", "UH", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ", "WDT", "WP", "WP$", "WRB"]
+
+-- parseWord :: String -> Maybe POS
+-- parseWord str = let (_,_,_,res) = ( str =~ "(.+)\\/(.+)" :: (String, String, String,[String]) ) in 
+--     case res of 
+--         [a, b]  -> Just $ (a, [b])
+--         _       -> Nothing
 
 
 parseWord :: String -> Maybe POS
-parseWord str = let (_,_,_,res) = ( str =~ "(.+)\\/(.+)" :: (String, String, String,[String]) ) in 
-    case res of 
-        [a, b]  -> Just $ (a, b)
-        _       -> Nothing
+parseWord str = 
+    case splitRegex (mkRegex "\\/") $ subRegex (mkRegex "\\\\/") str "****" of 
+        [a,b] -> Just $ (subRegex (mkRegex "\\*\\*\\*\\*") a "/", b') where b' = if '|' `elem` b then splitRegex (mkRegex "\\|") b else [b]
+        (a:b) -> Just $ (subRegex (mkRegex "\\*\\*\\*\\*") a "/", b) `debug` (a ++ show b)
+        rest  -> Nothing `debug` (show rest)
 
 parseSentence :: String -> Maybe Sentence
-parseSentence = mapM parseWord . words where
+parseSentence = mapM parseWord . words
 
 parseSentences :: String -> Maybe [Sentence]
-parseSentences = mapM parseSentence . filter (""/=) . splitRegex (mkRegex "=+") . (\x -> subRegex (mkRegex "\n|\\[|\\]") x "")
+parseSentences = mapM parseSentence . filter (not . empty) . splitRegex (mkRegex "=+") . (\x -> subRegex (mkRegex "\n|\\[|\\]") x " ")
+    where empty = (\x -> subRegex (mkRegex "\n| ") x "" == "")
 
 parseFile :: FilePath -> IO (Maybe [Sentence])
 parseFile f = withFile f ReadMode (\h -> hGetContents h >>= \contents -> 
@@ -47,16 +63,48 @@ getAllDataFilePathsRec f = do
     -- add the ".POS" files to the one returned by ret
     return $ (map ((f ++ "/") ++ ) $ filter (\path -> path =~ ".+\\.POS" :: Bool) allFiles) ++ ret
 
+
+save :: String -> FilePath -> IO ()
+save x f = writeFile f x
+
+
 main = do
-    files <- getAllDataFilePathsRec "./data/"
-    print files
+{-    files <- getAllDataFilePathsRec "./data/"
+    -- files <- getAllDataFilePaths "."
     parsed <- parseFiles files
     case parsed of
         Just r -> do
-            print $ "Probs"
-            print $ pAllTags x
-            where x = map startEndTags r
-        Nothing -> print "parse failed!!!!"
-    -- do 
-    --     r <- parseFiles ["WSJ_0200.POS", "WSJ_0201.POS"]
-    --     print r
+            writeFile "tCount.txt" (tagCountMapToStr tMap)
+            writeFile "ttCount.txt" (strStrCountMapToStr tTMap)
+            writeFile "wtCount.txt" (strStrCountMapToStr wTMap)
+
+            where 
+                x = map startEndTags r
+                wTMap = genWordTagOccsMap x
+                tTMap = genTagTagOccsMap x
+                tMap = genTagOccsMap x
+                -- pTT = genTagTagProbFun tMap tTMap
+                -- pWT = genWordTagProbFun tMap wTMap
+                -- tags = Map.keys $ tMap
+        Nothing -> print "parse failed!!!!"-}
+
+    tMapStr <- readFile "tCount.txt"
+    ttMapStr <- readFile "ttCount.txt"
+    wtMapStr <- readFile "wtCount.txt"
+
+    let
+        wTMap = strToStrStrCountMap wtMapStr
+        tTMap = strToStrStrCountMap ttMapStr
+        tMap = strToTagCountMap tMapStr
+        pTT = genTagTagProbFun tMap tTMap
+        pWT = genWordTagProbFun tMap wTMap
+        tags = M.keys $ tMap 
+        i = initScore pWT pTT "he" tags in
+        
+        -- putStr $ (show $ initScore pWT pTT "he" tags) ++ "\n\n" ++ (show $ last $ sortBy (comparing snd) $ M.toList $ initScore pWT pTT "he" tags)
+
+        -- putStr $ (show $ last $ sortBy (comparing snd) $ [(t, pWT ("he",t)) | t <- tags]) ++ "\n\n\n" ++ (show $ last $ sortBy (comparing snd) $ [(t, pWT ("was",t)) | t <- tags])  ++ "\n\n\n" ++ (show [(t, pTT ("VBD",t)) | t <- tags])
+        -- putStr $ show $ pWT ("he", "NN")
+
+        putStr $ show $ viterbi tags pWT pTT (splitOn " " "he was a sandwich .")
+
